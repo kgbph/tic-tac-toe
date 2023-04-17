98dbc7997330c373def027020e914764a2ab20aa
Kevin Bernardo <kgb.acct.personal@gmail.com>
2023-04-17 17:59:50 +0800

    create basic layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    {{ __('Start') }}
+</button>
+@endsection
