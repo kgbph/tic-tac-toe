7d749e4c4e1d04959948e6ac2c1bdbdf38f651ca
Kevin Bernardo <kgb.acct.personal@gmail.com>
2023-04-17 19:26:58 +0800

    disable cell on player input

--- resources/views/components/cell.blade.php
+++ resources/views/components/cell.blade.php
@@ -9,7 +9,8 @@
     <input type="hidden" name="player" value="1">
 
     <button type="submit"
-        class="w-32 h-32 bg-gray-900 flex justify-center items-center text-3xl text-white font-bold">
+        class="w-32 h-32 bg-gray-900 flex justify-center items-center text-3xl text-white font-bold"
+        @disabled($cell->getPlayer())>
         @switch($cell->getPlayer())
             @case(1)
                 {{ __('X') }}
